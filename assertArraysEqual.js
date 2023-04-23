3a22968e6c6bd4aabcdcacc015c9bbcabf697561
kevinrings <wnkevin@gmail.com>
2023-04-23 16:14:34 -0700

    use eqArrays function for array comparison

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -17,3 +17,9 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+// The message will be similar to that of assertEqual. In fact, you could refer to the code for assertEqual to help you implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // Output: ✅ Assertion Passed: 1,2,3 === 1,2,3
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // Output: 🛑 Assertion Failed: 1,2,3 !== 3,2,1
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Output: ✅ Assertion Passed: 1,2,3 === 1,2,3
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Output: 🛑 Assertion Failed: 1,2,3 !== 1,2,3
