c97e0d704a0c70f8311fed547bccff61a5c946a7
kevinrings <wnkevin@gmail.com>
2023-04-22 23:20:44 -0700

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+
+/*
+// FUNCTION IMPLEMENTATION
+const sum = function(a, b) { 
+  return a + b;
+}
+
+// TEST CODE
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+*/
