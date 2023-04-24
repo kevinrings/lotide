d7ec77c3aee3d398a304d83b9384e8cf6dc7f369
kevinrings <wnkevin@gmail.com>
2023-04-23 17:52:38 -0700

    flatt array

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = function(source) {
+  let flatt = [];
+  for (let element of source) {
+    if (Array.isArray(element)) {
+      for (let item of element) {
+        flatt.push(item);
+      }
+      continue;
+    }
+    flatt.push(element);
+  }
+  return flatt;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
