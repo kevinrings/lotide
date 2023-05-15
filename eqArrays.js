d80df029f657a575dce424cca1d89397991f448d
kevinrings <wnkevin@gmail.com>
2023-05-14 22:33:01 -0700

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -17,6 +19,10 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+
+module.exports = eqArrays;
+
+/*
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
@@ -24,3 +30,4 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+*/
